09bf2138db508e396011d6d0591ce7a374f419c0
TrshDesigns <trshdesigns@outlook.com>
2021-09-21 07:19:42 -0300

    changes

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,72 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
+/*save all the files */
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'script',
+	new workbox.strategies.CacheFirst()
+	
+);
 
 workbox.routing.registerRoute(
-	({request}) => request.destination === 'image',
-	new workbox.strategies.NetworkFirst()
+	({request}) => request.destination === 'style',
+	new workbox.strategies.CacheFirst()
 	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'document',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'font',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'audio',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'video',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'manifest',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'document',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'font',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'audio',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'video',
+	new workbox.strategies.CacheFirst()
+	
+);
+
+workbox.routing.registerRoute(
+	({request}) => request.destination === 'manifest',
+	new workbox.strategies.CacheFirst()
 );
